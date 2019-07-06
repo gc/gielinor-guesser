e6612b2b698c62ce85a68133f75607a0e9084150
gc <gc@users.noreply.github.com>
2019-07-06 16:06:41 +1000

    revamp

--- src/components/FloatingPanel.js
+++ src/components/FloatingPanel.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 class FloatingPanel extends Component {
-  render() {
-    return (
-      <div id="floatingPanel" className="rsBorder" style={{ backgroundImage: `url(${this.props.location.image})`}}>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div
+				id="floatingPanel"
+				className="rsBorder"
+				style={{ backgroundImage: `url(${this.props.location.image})` }}
+			/>
+		);
+	}
 }
 
 export default FloatingPanel;
